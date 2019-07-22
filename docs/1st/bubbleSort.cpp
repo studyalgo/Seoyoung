b6ed308d316830f2cb2f4566bba9037e5aae039f
Seo young Bae <seoyoungbae@Seoui-MacBook.local>
2019-07-22 19:08:15 +0900

    버블정렬 코드 수정

--- docs/1st/bubbleSort.cpp
+++ docs/1st/bubbleSort.cpp
@@ -7,7 +7,7 @@ int main()
 
     for(i = 0; i < 10; i++){
         for(j = 0; j < 9-i; j++){
-            if(array[j+1] > array[j]){
+            if(array[j] > array[j+1]){
                 temp = array[j];
                 array[j] = array[j+1];
                 array[j+1] = array[j];
