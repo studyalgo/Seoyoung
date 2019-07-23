291462189052802cb5e0c2ab0021bc9d790b6ac1
Seo young Bae <seoyoungbae@Seoui-MacBook.local>
2019-07-23 16:18:42 +0900

    인터넷 동영상보고 수정한 코드

--- docs/1st/quickSort.cpp
+++ docs/1st/quickSort.cpp
@@ -17,48 +17,43 @@ int main()
 void quickSort(int start, int end)
 {
     //재귀함수에서 빠져나갈 조건
-    if(start == end) return;
+    if(start >= end) return;    //'=='보다는 '>='로 범위를 더 넒게 잡아주는 것이 안전
 
-    int pivot, min, max;    //인덱스값
+    int pivot, i, j;    //인덱스값
     int temp;   //스와핑할 때 값 임시저장용
 
     pivot = start; // 제일 앞에 있는 값을 기준값으로.
+    i = start + 1;
+    j = end;
 
-    while(1)
+    while(i <= j)
     {
-        //왼쪽에서부터 기준값보다 큰 값을 찾는다.
-        for(int i = start + i; i <= end; i++){
-            if(array[i] > array[pivot]){
-                max = i;
-                break;
-                }
+        //왼쪽에서부터 기준값보다 큰 값을 만날 때 까지 반복.
+        while(array[i] <= array[pivot]){
+            i++;
         }
-        //오늘쪽에서부터 기준값보다 작은 값을 찾는다.
-        for(int i = end; i > pivot; i--){
-            if(array[i] < array[pivot]){
-                min = i;
-                break;
-                }
+        //오늘쪽에서부터 기준값보다 작은 값을 만날 때 까지 반복.
+        while(array[j] >= array[pivot] && j > start){
+            j--;
         }
         //엇갈린 경우(위 과정에서 찾은 작은값이 큰값보다 왼쪽에 있을 때)
-        if(min<max)
+        if(i > j)
         {
             //작은값과 기준값의 자리를 바꾸고 기준값기준 왼쪽과 오른쪽 데이터값들에도 퀵정렬을 적용한다.
             temp = array[pivot];
-            array[pivot] = array[min];
-            array[min] = temp;
-            // 왼쪽 정렬
-            quickSort(start, pivot-1);
-            //오른쪽 정렬
-            quickSort(pivot+1, end);
-            break;
+            array[pivot] = array[j];
+            array[j] = temp;
         }
         //엇갈리지 않은 경우
         else
         {
-            temp = array[min];
-            array[min] = array[max];
-            array[max] = temp;
-        }    
+            temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+        // 왼쪽 정렬
+        quickSort(start, pivot-1);
+        //오른쪽 정렬
+        quickSort(pivot+1, end);
     }
 }
