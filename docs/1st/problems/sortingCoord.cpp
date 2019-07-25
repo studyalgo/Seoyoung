4626a0e1b756a03d995cd1533bab5f09eac56e23
Seo young Bae <seoyoungbae@Seoui-MacBook.local>
2019-07-26 03:29:03 +0900

    디버깅

--- docs/1st/problems/sortingCoord.cpp
+++ docs/1st/problems/sortingCoord.cpp
@@ -1,26 +1,27 @@
 #include <iostream>
 using namespace std;
 
-void quickSortY(dot* array, int start, int end);
-
 //좌표 위의 점
 struct dot{
     int x;
     int y;
 };
 
+void quickSortY(struct dot* array, int start, int end);
+
 int main()
 {
     int n;  //좌표의 개수
     dot temp;   //for swapping
     cin>>n;
-    dot array[n];
+    dot *array = new dot[n];
     for(int i = 0; i < n; i++){
         cin>>array[i].x;
         cin>>array[i].y;
     }
     //y를 기준으로 퀵정렬
     quickSortY(array, 0, n);
+
     //y좌표가 같을 경우 x좌표 기준으로 버블정렬
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n-1; j++){
@@ -37,6 +38,9 @@ int main()
         cout<<array[i].x<<" ";
         cout<<array[i].y<<endl;
     }
+
+    delete[] array;
+    return 0;
 }
 
 void quickSortY(dot* array,int start, int end){
