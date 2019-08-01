7c1fa620336b20fc3067c94dc86707e3d35e0e96
Seo young Bae <seoyoungbae@Seoui-MacBook.local>
2019-08-01 19:51:31 +0900

    'k번째수' 코드 추가/터미널에서는 돌아가는데 백준에서는 컴파일에러남

---
+++ docs/2nd/problems/kNum.cpp
@@ -0,0 +1,39 @@
+/*-------------------------------------------------------------------------
+<문제>
+
+-------------------------------------------------------------------------*/
+#include <iostream>
+
+using namespace std;
+
+void Q(int i, int j, int k);
+
+int* arr;
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+
+    arr = new int[n];
+    int a, b, c;
+
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    
+    for(int i = 0; i < m; i++){
+        cin >> a >> b >> c;
+        Q(a, b, c);
+    }
+    return 0;
+}
+
+void Q(int i, int j, int k){
+    int len = j - i + 1;
+    int sub[len];
+    for(int x = 0; x < len; x++){
+        sub[x] = arr[x+i-1];
+    }
+    sort(sub, sub+len);
+    cout << sub[k-1] << endl;
+}
