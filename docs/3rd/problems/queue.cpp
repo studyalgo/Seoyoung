5c33ae063edca11251e3d7736931bd5e66ff55c6
Seo young Bae <seoyoungbae@Seoui-MacBook.local>
2019-08-08 18:43:26 +0900

    큐 코드 추가

---
+++ docs/3rd/problems/queue.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string cmd;
+int param;
+int sz;
+int emp;
+int front;
+int back;
+
+int main(){
+    //변수 초기화
+    sz = 0;
+    emp = 1;
+    front = 0;
+    back = 0;
+
+    //명령어 입력 횟수
+    int n;
+    cin >> n;
+
+    int* queue = new int[10000];
+
+    //n번만큼 실행
+    while(n>0){
+        cin >> cmd;
+        if(cmd == "push"){
+            cin >> param;
+            queue[back] = param;
+            back++;
+            sz++;
+            emp = 0;
+        }
+        else if(cmd == "pop"){
+            if(sz == 0) cout << "-1";
+            else {
+                cout << queue[front];
+                queue[front] = 0;
+                front++;
+                sz--;
+            }
+        }
+        else if(cmd =="size"){
+            cout << sz;
+        }
+        else if(cmd =="empty"){
+            if(sz == 0) cout << "-1";
+            else cout << "0";
+        }
+        else if(cmd =="front"){
+            if(sz == 0) cout << "-1";
+            else cout << queue[front];
+        }
+        else if(cmd =="back"){
+            if(sz == 0) cout << "-1";
+            else cout << queue[back];
+        }
+        n--;
+    }
+
+    return 0;
+}
