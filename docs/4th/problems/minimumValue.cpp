19d6e4d5e81a587edeab1266c903bab241bce6b0
Seo young Bae <seoyoungbae@192.168.nate.com>
2019-08-16 22:16:44 +0900

    또 'segmentation fault:11'...

--- docs/4th/problems/minimumValue.cpp
+++ docs/4th/problems/minimumValue.cpp
@@ -4,10 +4,14 @@
 
 using namespace std;
 
+int init(vector<int> &a, vector<int> &tree, int node, int start, int end);
+int findMinimum(vector<int> tree, int node, int start, int end, int left, int right);
+
 int main(){
     int n, m;
     int height, size;
     int num;
+    int a, b;
 
     cin >> n >> m;
 
@@ -27,19 +31,27 @@ int main(){
     init(arr, tree, 1, 0, n-1);
 
     while(m>0){
-
+        cin >> a >> b;
+        cout << findMinimum(tree, 1, 0, n-1, a-1, b-1);
         m--;
     }
     return 0;
 }
 int init(vector<int> &a, vector<int> &tree, int node, int start, int end){
+    int left, right;
     if(start == end){
         return tree[node] = a[start];
     } else{
-        return tree[node] = init(a, tree, node*2, start, (start+end)/2) + init(a, tree, node*2+1, (start+end)/2 + 1, end);
+        left = init(a, tree, node*2, start, (start+end)/2);
+        right = init(a, tree, node*2+1, (start+end)/2 + 1, end);
+        return tree[node] = left < right? left : right;
     }
 }
 
-int findMinimum(){
-
+int findMinimum(vector<int> tree, int node, int start, int end, int left, int right){
+    if(left > end || right < start) return INT_MAX;
+    if(left >= start && right <= end) return tree[node];
+    left = findMinimum(tree, node*2, start, (start+end)/2, left, right);
+    right = findMinimum(tree, node*2+1, (start+end)/2 + 1, end, left, right);
+    return left < right? left : right;
 }
