9e93cb55c2ace206b6f99b7cab2e434a5cfaa913
Seo young Bae <seoyoungbae@192.168.nate.com>
2019-08-16 22:43:50 +0900

    findMinimum 함수 수정

--- docs/4th/problems/minimumValue.cpp
+++ docs/4th/problems/minimumValue.cpp
@@ -17,7 +17,7 @@ int main(){
 
     //세그먼트 트리의 높이와 사이즈
     height = ceil(log2(n));
-    size = (1 >> (height+1));
+    size = (1 << (height+1));
 
     //배열과 트리생성 및 초기화
     vector<int> arr;
@@ -32,7 +32,7 @@ int main(){
 
     while(m>0){
         cin >> a >> b;
-        cout << findMinimum(tree, 1, 0, n-1, a-1, b-1);
+        cout << findMinimum(tree, 1, 0, n-1, a-1, b-1) << '\n';
         m--;
     }
     return 0;
@@ -51,7 +51,11 @@ int init(vector<int> &a, vector<int> &tree, int node, int start, int end){
 
 int findMinimum(vector<int> tree, int node, int start, int end, int left, int right){
     if(left > end || right < start) return 1e9;
-    if(left >= start && right <= end) return tree[node];
+    if(left >= start && right <= end){
+        left = findMinimum(tree, node*2, start, (start+end)/2, left, right);
+        right = findMinimum(tree, node*2+1, (start+end)/2 + 1, end, left, right);
+        return left < right? left : right;
+    }
     left = findMinimum(tree, node*2, start, (start+end)/2, left, right);
     right = findMinimum(tree, node*2+1, (start+end)/2 + 1, end, left, right);
     return left < right? left : right;
