36bd4a6f7e0c810b002bce8f398bd1f1d13c5234
Bailey <bsy2658@gmail.com>
2019-09-08 20:46:14 +0900

    최솟값최댓값 진행중..

--- docs/4th/minmaxValue.cpp
+++ docs/4th/minmaxValue.cpp
@@ -15,12 +15,16 @@ int main(){
     //vector를 이용하기 위한 tmp, 배열과 트리.
     int tmp;
     vector<int> arr;
-    vector<int> tree;
+    vector<int> minTree;
+    vector<int> maxTree;
 
     for(int i = 0; i < n; i++){ 
         cin >> tmp;
         arr.push_back(tmp);
     }
+
+    initMin(arr, minTree, 1, 0, n-1);
+    initMax(arr, maxTree, 1, 0, n-1);
 
     while(m > 0)
     {
@@ -30,11 +34,24 @@ int main(){
     return 0;
 }
 
-int init(vector<int> &arr, vector<int> &tree, int node, int start, int end){
+int initMin(vector<int> &arr, vector<int> &tree, int node, int start, int end){
+    int l, r;
     if(start == end){
         return tree[node] = arr[start];
     } else{
-        return tree[node] = init(arr, tree, node*2)
+        l = initMin(arr, tree, node*2, start, (start+end)/2);
+        r = initMin(arr, tree, node*2+1, (start+end)/2 + 1, end);
+        return tree[node] = l < r? l : r; 
     }
+}
 
+int initMax(vector<int> &arr, vector<int> &tree, int node, int start, int end){
+    int l, r;
+    if(start == end){
+        return tree[node] = arr[start];
+    } else{
+        l = initMax(arr, tree, node*2, start, (start+end)/2);
+        r = initMax(arr, tree, node*2+1, (start+end)/2 + 1, end);
+        return tree[node] = l < r? r : l; 
+    }
 }
