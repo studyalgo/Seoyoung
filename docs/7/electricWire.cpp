52b58836cc4838156f3f942e50021e99f84efe9a
Bailey <bsy2658@gmail.com>
2019-09-20 22:14:32 +0900

    벡터 배열 A를 100에서 n으로 수정

--- docs/7/electricWire.cpp
+++ docs/7/electricWire.cpp
@@ -5,6 +5,9 @@
 
 using namespace std;
 
+int LIS(vector< pair<int, int> > arr, int n);
+int lowerBound(vector<int> lis, int n);
+
 int LIS(vector< pair<int, int> > arr, int n){
     vector<int> lis;
     for(int i = 0; i < n; i++){
@@ -15,18 +18,20 @@ int LIS(vector< pair<int, int> > arr, int n){
     return lis.size();
 }
 
+//제공하는 lower_bound가 있었다..!! 그 친구는 바이너리여서 시간복잡도가 더 낮다. 그걸 쓰는게 더 효율적.
 int lowerBound(vector<int> lis, int n){
     for(int i = 0; i < lis.size()-1; i++){
         if(n < lis[i]) return i;
     }
     return (lis.size()-1);
 }
+
 int main(){
     //A전봇대와 B전봇대를 잇는 전깃줄의 개수
     int n;
     cin >> n;
     //A전봇대, B전봇대에 전깃줄이 연결되어 있는 번호 저장.
-    vector< pair<int, int> > A(100);
+    vector< pair<int, int> > A(n);
 
     //전깃줄 연결관계 입력받기
     for(int i = 0; i < n; i++){
@@ -34,12 +39,12 @@ int main(){
     }
 
     //pair의 첫번째 값을 기준으로 정렬
-    sort(A.begin(), A.begin()+n);
+    sort(A.begin(), A.end());
     //그러면 두번째 값을 기준으로 하려면 어떡하지?
 
 
     //pair의 두번째 값을 기준으로 lis를 구한다.
-    cout << LIS(A, n);
+    cout << n-LIS(A, n) << '\n';
     
 
     return 0;
