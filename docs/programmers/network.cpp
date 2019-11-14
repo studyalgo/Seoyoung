294e681f5505414fa035fd0314a30375f145c825
Bailey <bsy2658@gmail.com>
2019-11-14 21:58:48 +0900

    성공

--- docs/programmers/network.cpp
+++ docs/programmers/network.cpp
@@ -5,8 +5,8 @@
 using namespace std;
 
 void checkConn(int n, int i, vector<vector<int> > computers, vector<int>& visited){
-    if(visited[i] == 1) return;
     visited[i] = 1;
+
     for(int j = 0; j < n; j++){
         if(j==i) continue; //자기자신은 건너뛴다.
         if(visited[j] == 0 && computers[i][j] == 1){
@@ -21,14 +21,9 @@ int solution(int n, vector<vector<int> > computers) {
     vector<int> visited(n, 0);
     for(int i = 0; i < n; i++){
         if(visited[i] == 1) continue;
-
         visited[i] = 1;
         checkConn(n, i, computers, visited);
         answer++;
-        
-        cout << "i: " << i << "      answer:" << answer << '\n';
-        cout << "visited["<<i<<"] : " << visited[i] << '\n';
-
     } 
     return answer;
 }
