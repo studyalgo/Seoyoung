156b0678b322f4911cae8e0abe77a806375f5ac9
Bailey <bsy2658@gmail.com>
2019-11-14 21:22:21 +0900

    수정

--- docs/programmers/network.cpp
+++ docs/programmers/network.cpp
@@ -4,55 +4,34 @@
 
 using namespace std;
 
-void checkConn(int n, int i, vector<int>* visited);
+#include <string>
+#include <vector>
+#include <queue>
 
-int main(){
-    int n; //컴퓨터 개수
-    cin >> n; // 컴퓨터 개수 입력
+using namespace std;
 
-    vector<vector<int>> computers(n, vector<int>(n, 0));//컴퓨터 연결 자료구조
-    
-    //컴퓨터 연결 자료구조 입력받음
-    //입력정보 들어가는 순서
-    // 1 2 ... n
-    // n+1 ... 2n
-    //2n+1 ... 3n
-    for(int i = 0; i < n; i++){//행
-        for(int j = 0; j < n; j++){//열
-            cin >> computers[i][j];
-        }
-    }
-    
-
-
-    int answer = solution(n, computers);
-    cout << answer << '\n';
-    return 0;
-}
-
-int solution(int n, vector<vector<int>> computers){
-    int answer = 0;
-
-    vector<int> visited(n, 0);
-    for(int i = 0; i < n; i++){
-        if(visited[i] == 1) continue;
-
-        visited[i] = 1;
-        answer++;
-        checkConn(n, i, computers, &visited);
-    } 
-
-    return answer;
-}
-
-void checkConn(int n, int i, vector<vector<int>> computers, vector<int>* visited){
+void checkConn(int n, int i, vector<vector<int>> computers, vector<int>& visited){
     if(visited[i] == 1) return;
     
     visited[i] = 1;
     for(int j = 0; j < n; i++){
         if(j==i) continue; //자기자신은 건너뛴다.
         if(visited[j] == 0 && computers[i][j] == 1){
-            checkConn(n, j, computers, &visited);
+            checkConn(n, j, computers, visited);
         }
     }
+}
+
+int solution(int n, vector<vector<int>> computers) {
+    int answer = 0;
+    vector<int> visited(n, 0);
+    for(int i = 0; i < n; i++){
+        if(visited[i] == 1) continue;
+        else{
+            visited[i] = 1;
+            answer++;
+            checkConn(n, i, computers, visited);
+        }
+    } 
+    return answer;
 }
