376b6d00bb026fab53ac1752f261ea95a4427bac
Bailey <bsy2658@gmail.com>
2019-11-14 21:11:59 +0900

    오류 수정

--- docs/programmers/network.cpp
+++ docs/programmers/network.cpp
@@ -36,25 +36,23 @@ int solution(int n, vector<vector<int>> computers){
     vector<int> visited(n, 0);
     for(int i = 0; i < n; i++){
         if(visited[i] == 1) continue;
-        else{
-            visited[i] = 1;
-            answer++;
-            checkConn(n, i, &visited);
-        }
+
+        visited[i] = 1;
+        answer++;
+        checkConn(n, i, computers, &visited);
     } 
 
     return answer;
 }
 
-void checkConn(int n, int i, vector<int>* visited){
-    queue<int> conn;
-    int next;
+void checkConn(int n, int i, vector<vector<int>> computers, vector<int>* visited){
+    if(visited[i] == 1) return;
+    
+    visited[i] = 1;
     for(int j = 0; j < n; i++){
         if(j==i) continue; //자기자신은 건너뛴다.
-        if(visited[j] == 0) conn.push(j);
-    }
-    while(!conn.empty()){
-        next = conn.front();
-        checkConn(n, next, visited);
+        if(visited[j] == 0 && computers[i][j] == 1){
+            checkConn(n, j, computers, &visited);
+        }
     }
 }
